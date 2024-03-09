3d5b7a3029afe951a32ee098764fc0d3fa2547be
Euan Kemp <euank@euank.com>
2024-03-09 06:49:19 -0800

    Update

--- pascal/hardware-configuration.nix
+++ pascal/hardware-configuration.nix
@@ -37,6 +37,4 @@
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }

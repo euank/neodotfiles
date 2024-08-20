e8bb0ba524421eeada9ddbf2315a5620a6837d0c
Euan Kemp <euank@euank.com>
2024-08-20 18:48:39 +0900

    Update

--- enkidudu/configuration.nix
+++ enkidudu/configuration.nix
@@ -151,8 +151,8 @@ in
 
   virtualisation.docker.enable = true;
 
-  #virtualisation.virtualbox.host.enable = true;
-  #virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.libvirtd.enable = false;
 
   home-manager.users.esk = import ./home.nix;

3dae3bc2d73500a6fe41bbc127495fb855996e20
Euan Kemp <euank@euank.com>
2025-01-04 01:19:48 +0900

    Better model

--- pkgs/scripts.nix
+++ pkgs/scripts.nix
@@ -11,7 +11,7 @@
     #!/bin/sh -e
 
     selection="$(${pkgs.hacksaw}/bin/hacksaw -f "-i %i -g %g")"
-    ${pkgs.shotgun}/bin/shotgun $selection - | ${pkgs.llm}/bin/llm -a - "Please output your best guess at the Japanese characters in this image. Do not output any other text." | ${pkgs.xclip}/bin/xclip -selection clipboard
+    ${pkgs.shotgun}/bin/shotgun $selection - | ${pkgs.llm}/bin/llm -m "gpt-4o" -a - "Please output your best guess at the Japanese characters in this image. Do not output any other text." | ${pkgs.xclip}/bin/xclip -selection clipboard
   '';
 
 }

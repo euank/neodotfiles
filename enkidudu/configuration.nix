e31f4748da4a26c9738f5bf50b955ae6e18c72e5
Euan Kemp <euank@euank.com>
2025-07-30 02:29:50 +0900

    Try bluetooth

--- enkidudu/configuration.nix
+++ enkidudu/configuration.nix
@@ -15,11 +15,6 @@
   boot.extraModprobeConfig = ''
     options cfg80211 ieee80211_regdom="JP"
   '';
-  # drop internal bluetooth, use a usb dongle. The internal one doesn't work w/
-  # my headset.
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="0489", ATTRS{idProduct}=="e10a", ATTR{authorized}="0"
-  '';
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 

a51cf994c91a30e7054dbd7efb49d2a53fe707ab
Euan Kemp <euank@euank.com>
2026-03-12 12:27:55 +0900

    Update model

--- pkgs/scripts.nix
+++ pkgs/scripts.nix
@@ -15,7 +15,7 @@ in
 
   llm-ocr-area = pkgs.writeShellScriptBin "llm-ocr-area" ''
     selection="$(${slurp}/bin/slurp)"
-    ${grim}/bin/grim -g "$selection" - | ${llm}/bin/llm -m "gpt-4o" -a - "Please output your best guess at the Japanese characters in this image. Do not output any other text." | ${pkgs.wl-clipboard}/bin/wl-copy
+    ${grim}/bin/grim -g "$selection" - | ${llm}/bin/llm -m "gpt-5-mini" -a - "Please output your best guess at the Japanese characters in this image. Do not output any other text." | ${pkgs.wl-clipboard}/bin/wl-copy
   '';
 
 }

a9218fa8a3d260df672cf83eda52593cb23eb147
Euan Kemp <euank@euank.com>
2026-07-26 02:00:23 -0700

    Revert "Temporary travel"
    This reverts commit 5dba487d041c7d831ae6f454139a63b2a0f231a9.

--- demerzel/configuration.nix
+++ demerzel/configuration.nix
@@ -75,8 +75,6 @@ in
 
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
-  time.timeZone = pkgs.lib.mkForce "America/Los_Angeles";
-
   environment.systemPackages = with pkgs; [
     zfs
     keybase
